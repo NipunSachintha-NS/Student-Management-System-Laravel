02bbe66e1993a2c336902992ee61b5c44351e386
Nipun Sachintha <159788604+NipunSachintha-NS@users.noreply.github.com>
2024-08-30 22:01:27 +0530

    course page add

--- Project/routes/web.php
+++ Project/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ Route::get('/', function () {
 Route::resource('/students', StudentController::class);
 
 Route::resource('/teachers', TeacherController::class);
+
+Route::resource('/courses', CourseController::class);
 
 
  ?>
